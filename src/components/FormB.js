57f822234d3bb81d1556de7a39b42f5d5c84b038
Krishna Das <120264734+krishnaswgithub@users.noreply.github.com>
2023-05-21 20:17:09 +0530

    Update FormB.js

--- src/components/FormB.js
+++ src/components/FormB.js
@@ -1,35 +1,80 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
+// function FormB({ onSubmit, age }) {
+//     const [marvelShows, setMarvelShows] = useState("");
+
+//     const handleSubmit = (event) => {
+//     };
+
+
+
+//     return (
+//         <form id="marvel" >
+//             <h2>Form B</h2>
+//             <label>
+//                 Select Marvel Shows:
+//                 <select  >
+//                     <option value="">--Select--</option>
+//                     <option value="WandaVision">WandaVision</option>
+//                     <option value="The Falcon and the Winter Soldier">
+//                         The Falcon and the Winter Soldier
+//                     </option>
+//                     <option value="Loki">Loki</option>
+//                     <option value="What If...?">What If...?</option>
+//                 </select>
+//             </label>
+//             <br />
+//             <label>
+//                 Enter your age:
+//                 <input type="number" value={age} disabled />
+//             </label>
+//             <br />
+//             <button id="submit-marvel" type="submit">Submit</button>
+//         </form>
+//     );
+// }
+// export default FormB;
+
+
+import React, { useState } from "react";
+
 function FormB({ onSubmit, age }) {
-    const [marvelShows, setMarvelShows] = useState("");
+  const [selectedShow, setSelectedShow] = useState("");
 
-    const handleSubmit = (event) => {
-    };
+  const handleShowChange = (event) => {
+    setSelectedShow(event.target.value);
+  };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit(selectedShow);
+  };
 
-
-    return (
-        <form id="marvel" >
-            <h2>Form B</h2>
-            <label>
-                Select Marvel Shows:
-                <select  >
-                    <option value="">--Select--</option>
-                    <option value="WandaVision">WandaVision</option>
-                    <option value="The Falcon and the Winter Soldier">
-                        The Falcon and the Winter Soldier
-                    </option>
-                    <option value="Loki">Loki</option>
-                    <option value="What If...?">What If...?</option>
-                </select>
-            </label>
-            <br />
-            <label>
-                Enter your age:
-                <input type="number" value={age} disabled />
-            </label>
-            <br />
-            <button id="submit-marvel" type="submit">Submit</button>
-        </form>
-    );
+  return (
+    <form id="marvel" onSubmit={handleSubmit}>
+      <h2>Form B</h2>
+      <label>
+        Select Marvel Shows:
+        <select value={selectedShow} onChange={handleShowChange}>
+          <option value="">--Select--</option>
+          <option value="WandaVision">WandaVision</option>
+          <option value="The Falcon and the Winter Soldier">
+            The Falcon and the Winter Soldier
+          </option>
+          <option value="Loki">Loki</option>
+          <option value="What If...?">What If...?</option>
+        </select>
+      </label>
+      <br />
+      <label>
+        Enter your age:
+        <input type="number" value={age} disabled />
+      </label>
+      <br />
+      <button id="submit-marvel" type="submit" disabled={!selectedShow}>
+        Submit
+      </button>
+    </form>
+  );
 }
+
 export default FormB;
