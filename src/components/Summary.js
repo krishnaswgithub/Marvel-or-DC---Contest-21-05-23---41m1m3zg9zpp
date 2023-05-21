9236c805931c3fba0034ca505add0a1f7b847428
Krishna Das <120264734+krishnaswgithub@users.noreply.github.com>
2023-05-21 20:18:11 +0530

    Update Summary.js

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,16 +1,35 @@
-import { useState } from "react";
+// import { useState } from "react";
+// function Summary({ formData }) {
+//     return (
+//         <div id="summary">
+//             <h2>Summary</h2>
+//             <p>Form Type: </p>
+//             <p>Age: </p>
+
+//             <p>DC Shows: </p>
+
+//             <p>Marvel Shows: </p>
+
+//         </div>
+//     );
+// }
+// export default Summary;
+
+
+import React from "react";
+
 function Summary({ formData }) {
-    return (
-        <div id="summary">
-            <h2>Summary</h2>
-            <p>Form Type: </p>
-            <p>Age: </p>
+  const { formType, age, dcShow, marvelShow } = formData;
 
-            <p>DC Shows: </p>
-
-            <p>Marvel Shows: </p>
-
-        </div>
-    );
+  return (
+    <div id="summary">
+      <h2>Summary</h2>
+      <p>Form Type: {formType}</p>
+      <p>Age: {age}</p>
+      <p>DC Shows: {dcShow}</p>
+      <p>Marvel Shows: {marvelShow}</p>
+    </div>
+  );
 }
+
 export default Summary;
