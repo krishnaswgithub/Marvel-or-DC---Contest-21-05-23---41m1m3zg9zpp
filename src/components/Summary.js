512090fbae09b1a6bb5007ff6ae375e30137bbae
Krishna Das <120264734+krishnaswgithub@users.noreply.github.com>
2023-05-22 19:02:22 +0530

    Update Summary.js

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,35 +1,24 @@
-// import { useState } from "react";
-// function Summary({ formData }) {
-//     return (
-//         <div id="summary">
-//             <h2>Summary</h2>
-//             <p>Form Type: </p>
-//             <p>Age: </p>
-
-//             <p>DC Shows: </p>
-
-//             <p>Marvel Shows: </p>
-
-//         </div>
-//     );
-// }
-// export default Summary;
-
-
-import React from "react";
-
+import React, { useState } from "react";
 function Summary({ formData }) {
-  const { formType, age, dcShow, marvelShow } = formData;
+    return (
+        <div id="summary">
+            <h2>Summary</h2>
+            <p>Form Type: {formData.type}</p>
+            <p>Age: {formData.age}</p>
 
-  return (
-    <div id="summary">
-      <h2>Summary</h2>
-      <p>Form Type: {formType}</p>
-      <p>Age: {age}</p>
-      <p>DC Shows: {dcShow}</p>
-      <p>Marvel Shows: {marvelShow}</p>
-    </div>
-  );
+            {formData.type === "FormA" ? (
+                <p>DC Shows: {formData.show}</p>
+            ) : (
+                <p>Marvel Shows: {formData.show}</p>
+            )}
+
+            <button
+                id="start-over"
+                onClick={() => window.location.reload(true)}
+            >
+                Start Over
+            </button>
+        </div>
+    );
 }
-
 export default Summary;
