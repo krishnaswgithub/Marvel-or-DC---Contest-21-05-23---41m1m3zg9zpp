f0f9971bc076c40b0483fc851fa4eb4c10f55895
Krishna Das <120264734+krishnaswgithub@users.noreply.github.com>
2023-05-22 19:01:12 +0530

    Update FormA.js

--- src/components/FormA.js
+++ src/components/FormA.js
@@ -1,72 +1,47 @@
-// import { useState } from "react";
-
-// function FormA({ onSubmit, age }) {
-//     const handleSubmit = (event) => {
-//     };
-//     return (
-//         <form id="dc" onSubmit={handleSubmit}>
-//             <h2>Form A</h2>
-//             <label>
-//                 Select DC Shows:
-//                 <select  >
-//                     <option value="">--Select--</option>
-//                     <option value="The Flash">The Flash</option>
-//                     <option value="Arrow">Arrow</option>
-//                     <option value="Supergirl">Supergirl</option>
-//                     <option value="Legends of Tomorrow">Legends of Tomorrow</option>
-//                 </select>
-//             </label>
-//             <br />
-//             <label>
-//                 Enter your age:
-//                 <input type="number" value={age} disabled />
-//             </label>
-//             <br />
-//             <button id="submit-dc" type="submit">Submit</button>
-
-//         </form>
-//     );
-// }
-// export default FormA;
-
 import React, { useState } from "react";
+import Summary from "./Summary";
 
 function FormA({ onSubmit, age }) {
-  const [selectedShow, setSelectedShow] = useState("");
+    const [show, setShow] = useState("");
+    const [formData, setFormData] = useState("");
 
-  const handleShowChange = (event) => {
-    setSelectedShow(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    onSubmit(selectedShow);
-  };
-
-  return (
-    <form id="dc" onSubmit={handleSubmit}>
-      <h2>Form A</h2>
-      <label>
-        Select DC Shows:
-        <select value={selectedShow} onChange={handleShowChange}>
-          <option value="">--Select--</option>
-          <option value="The Flash">The Flash</option>
-          <option value="Arrow">Arrow</option>
-          <option value="Supergirl">Supergirl</option>
-          <option value="Legends of Tomorrow">Legends of Tomorrow</option>
-        </select>
-      </label>
-      <br />
-      <label>
-        Enter your age:
-        <input type="number" value={age} disabled />
-      </label>
-      <br />
-      <button id="submit-dc" type="submit" disabled={!selectedShow}>
-        Submit
-      </button>
-    </form>
-  );
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (show != "") {
+            setFormData({
+                type: "Form A",
+                age: age,
+                show: show,
+            });
+        }
+    };
+    return formData === "" ? (
+        <form id="dc" onSubmit={handleSubmit}>
+            <h2>Form A</h2>
+            <label>
+                Select DC Shows:
+                <select onChange={(e) => setShow(e.target.value)}>
+                    <option value="">--Select--</option>
+                    <option value="The Flash">The Flash</option>
+                    <option value="Arrow">Arrow</option>
+                    <option value="Supergirl">Supergirl</option>
+                    <option value="Legends of Tomorrow">
+                        Legends of Tomorrow
+                    </option>
+                </select>
+            </label>
+            <br />
+            <label>
+                Enter your age:
+                <input type="number" value={age} disabled />
+            </label>
+            <br />
+            <button id="submit-dc" type="submit">
+                Submit
+            </button>
+        </form>
+    ) : (
+        <Summary formData={formData} />
+    );
 }
-
 export default FormA;
